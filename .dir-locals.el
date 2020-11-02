3a65a6303d529793a634a03f785cf4ac65c2b094
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2020-11-02 03:56:36 +0100

    emacs settings for lockfiles

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+; https://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html
+((nil . ((create-lockfiles . nil)
+	 ))
+ (rjsx-mode . ((create-lockfiles . nil)
+               ))
+ (web-mode . ((create-lockfiles . nil)
+              ))
+ )
