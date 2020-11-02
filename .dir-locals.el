7ff05d5a5cee9521cbb8b1e5bb9111d5163eed5b
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2020-11-02 04:07:50 +0100

    emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,10 @@
 ; https://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html
 ((nil . ((create-lockfiles . nil)
 	 ))
+ (js-mode . ((create-lockfiles . nil)
+             ))
+ (js2-mode . ((create-lockfiles . nil)
+              ))
  (rjsx-mode . ((create-lockfiles . nil)
                ))
  (web-mode . ((create-lockfiles . nil)
